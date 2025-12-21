ae2bd6baa6f11bf21e7c979dae657b8b13026fb7
ki <kiripeng@outlook.com>
2025-12-21 16:41:59 +0800

    feat: ecdsa 工具使用

---
+++ lib/ecdsa/ecdsa.go
@@ -0,0 +1,55 @@
+package ecdsa
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+)
+
+func GenEcdsaPrivateKey() (privateKeyStr string, publicKeyStr string, err error) {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return "", "", err
+	}
+	ecPrivateKey, err := x509.MarshalECPrivateKey(privateKey)
+	if err != nil {
+		return "", "", err
+	}
+	privatePem := &pem.Block{
+		Type:  "EC PRIVATE KEY",
+		Bytes: ecPrivateKey,
+	}
+	privateKeyStr = string(pem.EncodeToMemory(privatePem))
+
+	publickKey, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return "", "", err
+	}
+
+	publickPem := &pem.Block{
+		Type:  "EC PUBLIC KEY",
+		Bytes: publickKey,
+	}
+	publicKeyStr = string(pem.EncodeToMemory(publickPem))
+
+	return privateKeyStr, publicKeyStr, nil
+}
+
+func EcdsaPublicKeyFromStr(str string) (*ecdsa.PublicKey, error) {
+	block, _ := pem.Decode([]byte(str))
+	if block == nil {
+		return nil, errors.New("ErrAuthKeyNotPem")
+	}
+	key, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+	publicKey, ok := key.(*ecdsa.PublicKey)
+	if !ok {
+		return nil, err
+	}
+	return publicKey, nil
+}
---
+++ lib/ecdsa/ecdsa_test.go
@@ -0,0 +1,20 @@
+package ecdsa_test
+
+import (
+	"lib/ecdsa"
+	"testing"
+)
+
+func TestGenEcdsaPrivateKey(t *testing.T) {
+	privte, public, err := ecdsa.GenEcdsaPrivateKey()
+	t.Log(privte, public, err)
+}
+
+func TestGenEcdsaPublicKey(t *testing.T) {
+	public := `-----BEGIN EC PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEMYW5N/LS9RRQJVpZcuVMpwnSvOT7
+CQNs8MNue3eMkqh1I+EDvlvA4yaGBp0H2ZEMXEU4gy8Npa+BNMhmnHUJHA==
+-----END EC PUBLIC KEY-----`
+	key, err := ecdsa.EcdsaPublicKeyFromStr(public)
+	t.Log(key, err)
+}
