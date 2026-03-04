19297013f977f7d7677a3f6cc396c8474e9d9cc3
skikda <kiripeng@outlook.com>
2026-03-04 21:11:40 +0800

    feat: 更改

--- ctl/excute/financial_state/init_params.go
+++ ctl/excute/financial_state/init_params.go
@@ -3,6 +3,6 @@ package financial_state
 import "github.com/spf13/cobra"
 
 func Init(rootCmd *cobra.Command) {
-	rootCmd.Flags().StringVarP(&FileStr, "file", "f", "", "文件地址")
-	rootCmd.Flags().StringVarP(&OutStr, "out", "o", "", "输出地址")
+	rootCmd.Flags().StringVarP(&FileStr, "file", "f", "", "file path")
+	rootCmd.Flags().StringVarP(&OutStr, "out", "o", "", "out path")
 }
